8b744686ba2c25454a319e1e2fd0b5892945aae4
Michael <mrbeach77@gmail.com>
2022-04-04 12:13:13 -0700

    update imgs

--- src/redux/blockchain/blockchainActions.js
+++ src/redux/blockchain/blockchainActions.js
@@ -51,10 +51,10 @@ export const connect = () => {
           method: "net_version",
         });
         // const NetworkData = await SmartContract.networks[networkId];
-        if (networkId == 1) {
+        if (networkId == 4) {
           const SmartContractObj = new Web3EthContract(
             SmartContract,
-            "0x688db0131c807a3495c23bc1b25726a76ea31f49"
+            "0xBdcb0e2ae2f3FE1bc0E9A853cDb988d7d90c5e18"
           );
           dispatch(
             connectSuccess({
